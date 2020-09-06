e705ab8783a8ea5d5a9067b95ec615ee4014b72f
Roland Levy <rolandjlevy@gmail.com>
2020-09-06 08:02:32 +0000

    Created basic solitaire grid shape

--- script.js
+++ script.js
@@ -4,9 +4,17 @@ const body = document.querySelector('body');
 const blockSize = getComputedStyle(body).getPropertyValue('--block-size');
 const rowLength = getComputedStyle(body).getPropertyValue('--row-length');
 
-let counter = 0;
-while (counter++ < rowLength * rowLength) {
+let index = 0;
+while (index < rowLength * rowLength) {
   const block = document.createElement('div');
   block.classList.add('block');
+  if (
+  (index % rowLength < 2 || index % rowLength >= rowLength - 2)
+   && (Math.floor(index / rowLength) < 2 || Math.floor(index / rowLength) >= rowLength - 2)
+   || (index % rowLength == Math.floor(rowLength / 2) && Math.floor(index / rowLength) == Math.floor(rowLength / 2)
+  )) {
+    block.classList.add('blank');
+  }
   container.appendChild(block);
+  index++;
 }
